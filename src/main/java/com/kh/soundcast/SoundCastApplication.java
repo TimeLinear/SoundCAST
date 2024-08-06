4b98dc9b344166b972239b5d5649cc37869fb5e8
TimeLinear36017 <TimeLinear36017@gmail.com>
2024-08-06 09:22:03 +0900

    First Push to Main Branch

---
+++ src/main/java/com/kh/soundcast/SoundCastApplication.java
@@ -0,0 +1,13 @@
+package com.kh.soundcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundCastApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundCastApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kh/soundcast/SoundCastApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kh.soundcast;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundCastApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
